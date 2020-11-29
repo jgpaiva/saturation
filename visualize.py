c62c6faa48f937e46d191c477601dcbf80fdaf36
João Paiva <jgpaiva@gmail.com>
2020-11-29 23:32:17 +0000

    Now with vegeta

--- visualize.py
+++ visualize.py
@@ -3,16 +3,15 @@ import numpy.random
 import matplotlib.pyplot as plt
 import math
 
-data = np.genfromtxt('www/data/raw_data.csv', delimiter=',',skip_header=1, dtype=[('Time','f8'),('Latency','f8')])
+data = np.genfromtxt('www/data/raw_data2.csv', delimiter=',', dtype=[('Time','f8'),('Latency','f8')])
 
 # Generate some test data
 x = data['Time']
 y = data['Latency']
 
 x_bins = math.ceil(max(x) - min(x))
-print(x_bins)
 # density=True, 
-heatmap, xedges, yedges = np.histogram2d(x, y, bins=[x_bins, 60], density=True)
+heatmap, xedges, yedges = np.histogram2d(x, y, bins=[x_bins, 100])
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
 fig = plt.figure()
